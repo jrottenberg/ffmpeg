f2b6ae0801af2eb10381dce3b36737561433c067
Julien Rottenberg <jrottenberg@users.noreply.github.com>
2018-02-26 07:45:08 -0800

    Snapshot (#97)
    * add snapshot version
    
    * snapshot support extend #96
    
    * not there anymore
    
    * Fix sourceforge URL
    
    * it's a | not a &&
    
    * don't download and pipe
    
    * build on my machine
    
    * use bzip2 for ffmpeg tarball

--- update.py
+++ update.py
@@ -2,14 +2,15 @@
 
 
 # Get latest release from ffmpeg.org
-import os, sys
+import os
+import sys
 import re
 import urllib2
 from distutils.version import StrictVersion
 
-MIN_VERSION='2.8'
+MIN_VERSION = '2.8'
 VARIANTS = ['ubuntu', 'alpine', 'centos', 'scratch']
-FFMPEG_RELEASES='https://ffmpeg.org/releases/'
+FFMPEG_RELEASES = 'https://ffmpeg.org/releases/'
 
 travis = []
 response = urllib2.urlopen(FFMPEG_RELEASES)
@@ -22,41 +23,45 @@ all_versions.sort(key=StrictVersion, reverse=True)
 version, all_versions = all_versions[0], all_versions[1:]
 
 last = version.split('.')
-keep_version = [version]
+keep_version = ['snapshot']
+
+keep_version.append(version)
 
 for cur in all_versions:
     if cur < MIN_VERSION:
         break
 
     tmp = cur.split('.')
-## Check Minor
+    # Check Minor
     if len(tmp) >= 2 and tmp[1].isdigit() and tmp[1] < last[1]:
         keep_version.append(cur)
         last = tmp
-## Check Major
+    # Check Major
     elif len(tmp) > 1 and tmp[0].isdigit() and tmp[0] < last[0]:
         keep_version.append(cur)
         last = tmp
 
 for version in keep_version:
     for variant in VARIANTS:
-        if variant == 'ubuntu':
-            dockerfile = 'docker-images/%s/Dockerfile' % version[0:3]
-            travis.append(' - VERSION=%s' % version[0:3])
+        if version == 'snapshot':
+            dockerfile = 'docker-images/%s/%s/Dockerfile' % (
+                version, variant)
+            travis.append(' - VERSION=%s VARIANT=%s' % (version, variant))
         else:
-            dockerfile = 'docker-images/%s/%s/Dockerfile' % (version[0:3], variant)
+            dockerfile = 'docker-images/%s/%s/Dockerfile' % (
+                version[0:3], variant)
             travis.append(' - VERSION=%s VARIANT=%s' % (version[0:3], variant))
 
         with open('templates/Dockerfile-env', 'r') as tmpfile:
-           env_content = tmpfile.read()
-        with open('templates/Dockerfile-template.' + variant , 'r') as tmpfile:
-           template = tmpfile.read()
+            env_content = tmpfile.read()
+        with open('templates/Dockerfile-template.' + variant, 'r') as tmpfile:
+            template = tmpfile.read()
         with open('templates/Dockerfile-run', 'r') as tmpfile:
-           run_content = tmpfile.read()
+            run_content = tmpfile.read()
         env_content = env_content.replace('%%FFMPEG_VERSION%%', version)
         docker_content = template.replace('%%ENV%%', env_content)
         docker_content = docker_content.replace('%%RUN%%', run_content)
-        ## OpenJpeg 2.1 is not supported in 2.8
+        # OpenJpeg 2.1 is not supported in 2.8
         if version[0:3] == '2.8':
             docker_content = docker_content.replace('--enable-libopenjpeg', '')
             docker_content = docker_content.replace('--enable-libkvazaar', '')
@@ -66,13 +71,13 @@ for version in keep_version:
             os.makedirs(d)
 
         with open(dockerfile, 'w') as dfile:
-          dfile.write(docker_content)
+            dfile.write(docker_content)
 
 
 with open('templates/travis.template', 'r') as tmpfile:
-   template = tmpfile.read()
+    template = tmpfile.read()
 travis = template.replace('%%VERSIONS%%', '\n'.join(travis))
 
 
 with open('.travis.yml', 'w') as travisfile:
-  travisfile.write(travis)
+    travisfile.write(travis)
