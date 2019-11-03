446da2ccd5921ac119d313e64cef2f0bfaf540dd
Eugene Medvedev <jidckii@gmail.com>
2019-11-03 21:16:00 +0500

    Add options to check skript

--- check_build_successful.sh
+++ check_build_successful.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [[ -z ${1} ]];then
+    STR_COUNT=1
+else
+    STR_COUNT=${1}
+fi
+
 PROJECT_DIR=$(dirname $0)
 LOG_FILES=$(find ${PROJECT_DIR} -name stdout | sort)
 
@@ -12,6 +18,6 @@ for FILE in ${LOG_FILES} ; do
         tail -n 1 ${FILE}
     else
         echo -e '\e[0;31m'"${FILE}"'\e[0m'
-        tail -n 1 ${FILE}
+        tail -n ${STR_COUNT} ${FILE}
     fi
 done
