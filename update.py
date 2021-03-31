743766727b770d3fc7867dc3d210d6ed551ef215
Pedro Diaz <p.diaz@wemersive.com>
2021-03-31 17:11:21 +0200

    Added VMAF for ffmpeg 4.3 and meson to build it.

--- update.py
+++ update.py
@@ -211,6 +211,9 @@ for version in keep_version:
         if version == "snapshot" or float(version[0:3]) >= 4.2:
             FFMPEG_CONFIG_FLAGS.append("--enable-libaribb24")
 
+        if ((template.find('meson') > 0) and (version == "snapshot" or float(version[0:3]) >= 4.3)):
+            FFMPEG_CONFIG_FLAGS.append("--enable-libvmaf")
+
         if (version == "snapshot" or int(version[0]) >= 3) and variant[
             "parent"
         ] == "vaapi":
