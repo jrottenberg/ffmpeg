b7cbc61639f7a9d899cf24363c4cd0f7f5f6aea3
Julien Rottenberg <julien@rottenberg.info>
2022-09-18 17:56:29 -0700

    quick module

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     name='pre_commit_placeholder_package',
     version='0.0.0',
     install_requires=['packaging'],
+    py_modules=['generate_dockerfile'],
     entry_points={
-        "console_scripts": ["generate_dockerfile = update:main"]
+        "console_scripts": ["generate_dockerfile = generate_dockerfile:main"]
         }
 )
