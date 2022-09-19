cd2458bce4dedf805ea08aab9f481fcad858ba56
Julien Rottenberg <julien@rottenberg.info>
2022-09-18 17:51:00 -0700

    move to pre-commit real hook

--- update.py
+++ update.py
@@ -32,44 +32,7 @@ VARIANTS = [
 FFMPEG_RELEASES = "https://ffmpeg.org/releases/"
 
 
-all_parents = sorted(set([sub["parent"] for sub in VARIANTS]))
-gitlabci = ["stages:\n  - lint\n"]
-azure = []
 
-
-for parent in all_parents:
-    gitlabci.append(f"  - {parent}\n")
-
-
-# Get latest release from ffmpeg.org
-with request.urlopen(FFMPEG_RELEASES) as conn:
-    ffmpeg_releases = conn.read().decode("utf-8")
-
-parse_re = re.compile(r"ffmpeg-([.0-9]+).tar.bz2.asc</a>\s+")
-all_versions = parse_re.findall(ffmpeg_releases)
-all_versions.sort(key=Version, reverse=True)
-
-version, all_versions = all_versions[0], all_versions[1:]
-
-SKIP_VARIANTS = {
-    "3.2": [
-        "alpine312",
-        "centos8",
-        "nvidia2004",
-        "scratch312",
-        "ubuntu2204",
-        "vaapi2204",
-    ],
-    "3.3": ["alpine38", "nvidia1604", "scratch38", "vaapi2204"],
-    "3.4": ["alpine38", "nvidia1604", "scratch38", "vaapi2204"],
-    "4.1": ["alpine38", "nvidia1604", "scratch38", "vaapi2204"],
-    "4.2": ["alpine38", "nvidia1604", "scratch38", "vaapi2204"],
-}
-
-last = version.split(".")
-keep_version = []
-
-keep_version.append(version)
 
 
 def get_shorten_version(version):
@@ -88,188 +51,235 @@ def get_major_version(version):
         return f"{major}"
 
 
-for cur in all_versions:
-    if cur < MIN_VERSION:
-        break
 
-    if get_shorten_version(cur) in SKIP_VERSIONS:
-        continue
-    tmp = cur.split(".")
-    # Check Minor
-    if len(tmp) >= 2 and tmp[1].isdigit() and tmp[1] < last[1]:
-        keep_version.append(cur)
-        last = tmp
-    # Check Major
-    elif len(tmp) > 1 and tmp[0].isdigit() and tmp[0] < last[0]:
-        keep_version.append(cur)
-        last = tmp
+def main():
 
-print("Preparing docker images for ffmpeg versions: ")
+    gitlabci = ["stages:\n  - lint\n"]
+    azure = []
 
-for version in keep_version:
-    print(version)
-    skip_variants = None
-    for k, v in SKIP_VARIANTS.items():
-        if version.startswith(k):
-            skip_variants = v
-    compatible_variants = [
-        v for v in VARIANTS if skip_variants is None or v["name"] not in skip_variants
-    ]
-    short_version = get_shorten_version(version)
-    major_version = get_major_version(version)
-    ver_path = os.path.join("docker-images", short_version)
-    if not Path(ver_path).is_dir():
-        os.makedirs(ver_path)
-    for existing_variant in os.listdir(ver_path):
-        if existing_variant not in compatible_variants:
-            shutil.rmtree(DIR_FORMAT_STR.format(short_version, existing_variant))
+    # Get latest release from ffmpeg.org
+    with request.urlopen(FFMPEG_RELEASES) as conn:
+        ffmpeg_releases = conn.read().decode("utf-8")
 
-    for variant in compatible_variants:
-        siblings = [
-            v["name"] for v in compatible_variants if v["parent"] == variant["parent"]
+    all_parents = sorted(set([sub["parent"] for sub in VARIANTS]))
+    for parent in all_parents:
+        gitlabci.append(f"  - {parent}\n")
+
+
+    parse_re = re.compile(r"ffmpeg-([.0-9]+).tar.bz2.asc</a>\s+")
+    all_versions = parse_re.findall(ffmpeg_releases)
+    all_versions.sort(key=Version, reverse=True)
+
+    version, all_versions = all_versions[0], all_versions[1:]
+
+    SKIP_VARIANTS = {
+        "3.2": [
+            "alpine312",
+            "centos8",
+            "nvidia2004",
+            "scratch312",
+            "ubuntu2204",
+            "vaapi2204",
+        ],
+        "3.3": ["alpine38", "nvidia1604", "scratch38", "vaapi2204"],
+        "3.4": ["alpine38", "nvidia1604", "scratch38", "vaapi2204"],
+        "4.1": ["alpine38", "nvidia1604", "scratch38", "vaapi2204"],
+        "4.2": ["alpine38", "nvidia1604", "scratch38", "vaapi2204"],
+    }
+
+    last = version.split(".")
+    keep_version = []
+
+    keep_version.append(version)
+
+
+
+    for cur in all_versions:
+        if cur < MIN_VERSION:
+            break
+
+        if get_shorten_version(cur) in SKIP_VERSIONS:
+            continue
+        tmp = cur.split(".")
+        # Check Minor
+        if len(tmp) >= 2 and tmp[1].isdigit() and tmp[1] < last[1]:
+            keep_version.append(cur)
+            last = tmp
+        # Check Major
+        elif len(tmp) > 1 and tmp[0].isdigit() and tmp[0] < last[0]:
+            keep_version.append(cur)
+            last = tmp
+
+    print("Preparing docker images for ffmpeg versions: ")
+
+    for version in keep_version:
+        print(version)
+        skip_variants = None
+        for k, v in SKIP_VARIANTS.items():
+            if version.startswith(k):
+                skip_variants = v
+        compatible_variants = [
+            v for v in VARIANTS if skip_variants is None or v["name"] not in skip_variants
         ]
-        is_parent = sorted(siblings, reverse=True)[0] == variant["name"]
-        dockerfile = IMAGE_FORMAT_STR.format(short_version, variant["name"])
-        gitlabci.append(
-            f"""
-{version}-{variant['name']}:
-  extends: .docker
-  stage: {variant['parent']}
-  variables:
-    MAJOR_VERSION: {major_version}
-    VERSION: "{short_version}"
-    LONG_VERSION: "{version}"
-    VARIANT: {variant['name']}
-    PARENT: "{variant['parent']}"
-    ISPARENT: "{is_parent}"
-"""
-        )
+        short_version = get_shorten_version(version)
+        major_version = get_major_version(version)
+        ver_path = os.path.join("docker-images", short_version)
+        if not Path(ver_path).is_dir():
+            os.makedirs(ver_path)
+        for existing_variant in os.listdir(ver_path):
+            if existing_variant not in compatible_variants:
+                shutil.rmtree(DIR_FORMAT_STR.format(short_version, existing_variant))
 
-        azure.append(
-            f"""
-      {variant["name"]}_{version}:
+        for variant in compatible_variants:
+            siblings = [
+                v["name"] for v in compatible_variants if v["parent"] == variant["parent"]
+            ]
+            is_parent = sorted(siblings, reverse=True)[0] == variant["name"]
+            dockerfile = IMAGE_FORMAT_STR.format(short_version, variant["name"])
+            gitlabci.append(
+                f"""
+    {version}-{variant['name']}:
+    extends: .docker
+    stage: {variant['parent']}
+    variables:
         MAJOR_VERSION: {major_version}
-        VERSION:  {short_version}
-        LONG_VERSION: {version}
-        VARIANT:  {variant["name"]}
-        PARENT: {variant["parent"]}
-        ISPARENT:  {is_parent}
-"""
-        )
-        with open(TEMPLATE_STR.format(variant["name"]), "r") as tmpfile:
-            template = tmpfile.read()
+        VERSION: "{short_version}"
+        LONG_VERSION: "{version}"
+        VARIANT: {variant['name']}
+        PARENT: "{variant['parent']}"
+        ISPARENT: "{is_parent}"
+    """
+            )
 
-        FFMPEG_CONFIG_FLAGS = [
-            "--disable-debug",
-            "--disable-doc",
-            "--disable-ffplay",
-            "--enable-fontconfig",
-            "--enable-gpl",
-            "--enable-libass",
-            "--enable-libbluray",
-            "--enable-libfdk_aac",
-            "--enable-libfreetype",
-            "--enable-libmp3lame",
-            "--enable-libopencore-amrnb",
-            "--enable-libopencore-amrwb",
-            "--enable-libopus",
-            "--enable-libtheora",
-            "--enable-libvidstab",
-            "--enable-libvorbis",
-            "--enable-libvpx",
-            "--enable-libwebp",
-            "--enable-libx264",
-            "--enable-libx265",
-            "--enable-libxcb",
-            "--enable-libxvid",
-            "--enable-libzmq",
-            "--enable-nonfree",
-            "--enable-openssl",
-            "--enable-postproc",
-            "--enable-shared",
-            "--enable-small",
-            "--enable-version3",
-            "--extra-libs=-ldl",
-            '--prefix="${PREFIX}"',
-        ]
-        CFLAGS = [
-            "-I${PREFIX}/include",
-        ]
-        LDFLAGS = [
-            "-L${PREFIX}/lib",
-        ]
+            azure.append(
+                f"""
+        {variant["name"]}_{version}:
+            MAJOR_VERSION: {major_version}
+            VERSION:  {short_version}
+            LONG_VERSION: {version}
+            VARIANT:  {variant["name"]}
+            PARENT: {variant["parent"]}
+            ISPARENT:  {is_parent}
+    """
+            )
+            with open(TEMPLATE_STR.format(variant["name"]), "r") as tmpfile:
+                template = tmpfile.read()
 
-        # OpenJpeg 2.1 is not supported in 2.8
-        if version[0:3] != "2.8":
-            FFMPEG_CONFIG_FLAGS.append("--enable-libopenjpeg")
-            FFMPEG_CONFIG_FLAGS.append("--enable-libkvazaar")
-        if version == "snapshot" or int(version[0]) > 3:
-            FFMPEG_CONFIG_FLAGS.append("--enable-libaom")
-            FFMPEG_CONFIG_FLAGS.append("--extra-libs=-lpthread")
+            FFMPEG_CONFIG_FLAGS = [
+                "--disable-debug",
+                "--disable-doc",
+                "--disable-ffplay",
+                "--enable-fontconfig",
+                "--enable-gpl",
+                "--enable-libass",
+                "--enable-libbluray",
+                "--enable-libfdk_aac",
+                "--enable-libfreetype",
+                "--enable-libmp3lame",
+                "--enable-libopencore-amrnb",
+                "--enable-libopencore-amrwb",
+                "--enable-libopus",
+                "--enable-libtheora",
+                "--enable-libvidstab",
+                "--enable-libvorbis",
+                "--enable-libvpx",
+                "--enable-libwebp",
+                "--enable-libx264",
+                "--enable-libx265",
+                "--enable-libxcb",
+                "--enable-libxvid",
+                "--enable-libzmq",
+                "--enable-nonfree",
+                "--enable-openssl",
+                "--enable-postproc",
+                "--enable-shared",
+                "--enable-small",
+                "--enable-version3",
+                "--extra-libs=-ldl",
+                '--prefix="${PREFIX}"',
+            ]
+            CFLAGS = [
+                "-I${PREFIX}/include",
+            ]
+            LDFLAGS = [
+                "-L${PREFIX}/lib",
+            ]
 
-        # LibSRT is supported from 4.0
-        if version == "snapshot" or int(version[0]) >= 4:
-            FFMPEG_CONFIG_FLAGS.append("--enable-libsrt")
+            # OpenJpeg 2.1 is not supported in 2.8
+            if version[0:3] != "2.8":
+                FFMPEG_CONFIG_FLAGS.append("--enable-libopenjpeg")
+                FFMPEG_CONFIG_FLAGS.append("--enable-libkvazaar")
+            if version == "snapshot" or int(version[0]) > 3:
+                FFMPEG_CONFIG_FLAGS.append("--enable-libaom")
+                FFMPEG_CONFIG_FLAGS.append("--extra-libs=-lpthread")
 
-        # LibARIBB24 is supported from 4.2
-        if version == "snapshot" or float(version[0:3]) >= 4.2:
-            FFMPEG_CONFIG_FLAGS.append("--enable-libaribb24")
-
-        if (template.find("meson") > 0) and (
-            version == "snapshot" or float(version[0:3]) >= 4.3
-        ):
-            FFMPEG_CONFIG_FLAGS.append("--enable-libvmaf")
-
-        if (version == "snapshot" or int(version[0]) >= 3) and variant[
-            "parent"
-        ] == "vaapi":
-            FFMPEG_CONFIG_FLAGS.append("--enable-vaapi")
-
-        # libavresample removed on v5, deprecated since v4.0
-        # https://github.com/FFmpeg/FFmpeg/commit/c29038f3041a4080342b2e333c1967d136749c0f
-        if float(version[0]) < 5:
-            FFMPEG_CONFIG_FLAGS.append("--enable-avresample")
-
-        if variant["parent"] == "nvidia":
-            CFLAGS.append("-I${PREFIX}/include/ffnvcodec")
-            CFLAGS.append("-I/usr/local/cuda/include/")
-            LDFLAGS.append("-L/usr/local/cuda/lib64")
-            LDFLAGS.append("-L/usr/local/cuda/lib32/")
-            FFMPEG_CONFIG_FLAGS.append("--enable-nvenc")
+            # LibSRT is supported from 4.0
             if version == "snapshot" or int(version[0]) >= 4:
-                FFMPEG_CONFIG_FLAGS.append("--enable-cuda")
-                FFMPEG_CONFIG_FLAGS.append("--enable-cuvid")
-                FFMPEG_CONFIG_FLAGS.append("--enable-libnpp")
-        cflags = '--extra-cflags="{0}"'.format(" ".join(CFLAGS))
-        ldflags = '--extra-ldflags="{0}"'.format(" ".join(LDFLAGS))
-        FFMPEG_CONFIG_FLAGS.append(cflags)
-        FFMPEG_CONFIG_FLAGS.append(ldflags)
-        FFMPEG_CONFIG_FLAGS.sort()
+                FFMPEG_CONFIG_FLAGS.append("--enable-libsrt")
 
-        COMBINED_CONFIG_FLAGS = " \\\n        ".join(FFMPEG_CONFIG_FLAGS)
+            # LibARIBB24 is supported from 4.2
+            if version == "snapshot" or float(version[0:3]) >= 4.2:
+                FFMPEG_CONFIG_FLAGS.append("--enable-libaribb24")
 
-        run_content = RUN_CONTENT.replace(
-            "%%FFMPEG_CONFIG_FLAGS%%", COMBINED_CONFIG_FLAGS
-        )
-        env_content = ENV_CONTENT.replace("%%FFMPEG_VERSION%%", version)
-        docker_content = template.replace("%%ENV%%", env_content)
-        docker_content = docker_content.replace("%%RUN%%", run_content)
+            if (template.find("meson") > 0) and (
+                version == "snapshot" or float(version[0:3]) >= 4.3
+            ):
+                FFMPEG_CONFIG_FLAGS.append("--enable-libvmaf")
 
-        d = os.path.dirname(dockerfile)
-        if not os.path.exists(d):
-            os.makedirs(d)
+            if (version == "snapshot" or int(version[0]) >= 3) and variant[
+                "parent"
+            ] == "vaapi":
+                FFMPEG_CONFIG_FLAGS.append("--enable-vaapi")
 
-        with open(dockerfile, "w") as dfile:
-            dfile.write(docker_content)
+            # libavresample removed on v5, deprecated since v4.0
+            # https://github.com/FFmpeg/FFmpeg/commit/c29038f3041a4080342b2e333c1967d136749c0f
+            if float(version[0]) < 5:
+                FFMPEG_CONFIG_FLAGS.append("--enable-avresample")
+
+            if variant["parent"] == "nvidia":
+                CFLAGS.append("-I${PREFIX}/include/ffnvcodec")
+                CFLAGS.append("-I/usr/local/cuda/include/")
+                LDFLAGS.append("-L/usr/local/cuda/lib64")
+                LDFLAGS.append("-L/usr/local/cuda/lib32/")
+                FFMPEG_CONFIG_FLAGS.append("--enable-nvenc")
+                if version == "snapshot" or int(version[0]) >= 4:
+                    FFMPEG_CONFIG_FLAGS.append("--enable-cuda")
+                    FFMPEG_CONFIG_FLAGS.append("--enable-cuvid")
+                    FFMPEG_CONFIG_FLAGS.append("--enable-libnpp")
+            cflags = '--extra-cflags="{0}"'.format(" ".join(CFLAGS))
+            ldflags = '--extra-ldflags="{0}"'.format(" ".join(LDFLAGS))
+            FFMPEG_CONFIG_FLAGS.append(cflags)
+            FFMPEG_CONFIG_FLAGS.append(ldflags)
+            FFMPEG_CONFIG_FLAGS.sort()
+
+            COMBINED_CONFIG_FLAGS = " \\\n        ".join(FFMPEG_CONFIG_FLAGS)
+
+            run_content = RUN_CONTENT.replace(
+                "%%FFMPEG_CONFIG_FLAGS%%", COMBINED_CONFIG_FLAGS
+            )
+            env_content = ENV_CONTENT.replace("%%FFMPEG_VERSION%%", version)
+            docker_content = template.replace("%%ENV%%", env_content)
+            docker_content = docker_content.replace("%%RUN%%", run_content)
+
+            d = os.path.dirname(dockerfile)
+            if not os.path.exists(d):
+                os.makedirs(d)
+
+            with open(dockerfile, "w") as dfile:
+                dfile.write(docker_content)
 
 
-with open("docker-images/gitlab-ci.yml", "w") as gitlabcifile:
-    gitlabcifile.write("".join(gitlabci))
+    with open("docker-images/gitlab-ci.yml", "w") as gitlabcifile:
+        gitlabcifile.write("".join(gitlabci))
 
-with open("templates/azure.template", "r") as tmpfile:
-    template = tmpfile.read()
-azure = template.replace("%%VERSIONS%%", "\n".join(azure))
+    with open("templates/azure.template", "r") as tmpfile:
+        template = tmpfile.read()
+    azure = template.replace("%%VERSIONS%%", "\n".join(azure))
 
 
-with open("docker-images/azure-jobs.yml", "w") as azurefile:
-    azurefile.write(azure)
+    with open("docker-images/azure-jobs.yml", "w") as azurefile:
+        azurefile.write(azure)
+
+
+if __name__ == "__main__":
+   exit (main())
