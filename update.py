5096d981d4cdfbcb84e2a14234b75a58193c57f0
Julien Rottenberg <julien@rottenberg.info>
2023-12-10 20:15:31 -0800

    cleanup old distros

--- update.py
+++ update.py
@@ -32,10 +32,9 @@ for v in json.loads(ffmpeg_releases):
 VARIANTS = [
     {"name": "ubuntu1804", "parent": "ubuntu"},
     {"name": "ubuntu2004", "parent": "ubuntu"},
-    {"name": "alpine313", "parent": "alpine"},
+    {"name": "alpine319", "parent": "alpine"},
     {"name": "centos7", "parent": "centos"},
-    {"name": "centos8", "parent": "centos"},
-    {"name": "scratch313", "parent": "scratch"},
+    {"name": "scratch", "parent": "scratch"},
     {"name": "vaapi1804", "parent": "vaapi"},
     {"name": "vaapi2004", "parent": "vaapi"},
     {"name": "nvidia2004", "parent": "nvidia"},
